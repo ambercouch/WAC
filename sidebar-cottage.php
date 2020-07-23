c1a382af4ec53bf684821f6fb7a5daa886e6932c
Kain <liamkainowen@gmail.com>
2020-07-23 11:09:56 +0100

    Adds cottages to sidebar, restyles slightly to git numbers within houses

--- sidebar-cottage.php
+++ sidebar-cottage.php
@@ -2,15 +2,15 @@
 		<h4>Select a Cottage</h4>
 		<ul>
       <li>
-        <a class="cerbyd" href="<?php bloginfo('url'); ?>unique-holiday-cottages/lloc-llo/">
-          <span class="bed-count">4</span>
-          Lloc Llo
+        <a class="nant" href="<?php bloginfo('url'); ?>unique-holiday-cottages/nant-y-plas/">
+          <span class="bed-count">2</span>
+          Nant Y Plas
         </a>
       </li>
       <li>
-        <a class="cerbyd" href="<?php bloginfo('url'); ?>unique-holiday-cottages/ty-gambo/">
-          <span class="bed-count">6</span>
-          Ty Gambo
+        <a class="cerbyd" href="<?php bloginfo('url'); ?>unique-holiday-cottages/lloc-llo/">
+          <span class="bed-count">4</span>
+          Lloc Llo
         </a>
       </li>
 			<li>
@@ -35,6 +35,30 @@
         <a class="cerbyd" href="<?php bloginfo('url'); ?>unique-holiday-cottages/ty-cerbyd/">
           <span class="bed-count">4</span>
           Ty Cerbyd
+        </a>
+      </li>
+      <li>
+        <a class="ysgol" href="<?php bloginfo('url'); ?>unique-holiday-cottages/yr-hen-ysgol/">
+          <span class="bed-count">4</span>
+          Yr Hen Ysgol
+        </a>
+      </li>
+      <li>
+        <a class="cerbyd" href="<?php bloginfo('url'); ?>unique-holiday-cottages/ty-gambo/">
+          <span class="bed-count">6</span>
+          Ty Gambo
+        </a>
+      </li>
+      <li>
+        <a class="bryn" href="<?php bloginfo('url'); ?>unique-holiday-cottages/bryn-hir/">
+          <span class="bed-count">8</span>
+          Bryn Hir
+        </a>
+      </li>
+      <li>
+        <a class="cilybebyll" href="<?php bloginfo('url'); ?>unique-holiday-cottages/">
+          <span class="bed-count">18</span>
+          Plas Cilybebyll
         </a>
       </li>
 		</ul>
